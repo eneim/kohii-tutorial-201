c9afa5ae0d3d08fb0068de94f5291d06156bfabb
eneim <1776230+eneim@users.noreply.github.com>
2020-06-28 10:55:45 +0900

    refactor: use PlayerView dynamically

--- app/src/main/java/kohii/tut/c201/VideoViewHolder.kt
+++ app/src/main/java/kohii/tut/c201/VideoViewHolder.kt
@@ -1,10 +1,10 @@
 package kohii.tut.c201
 
 import android.view.View
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.google.android.exoplayer2.ui.PlayerView
 
 class VideoViewHolder(itemView: View) : ViewHolder(itemView) {
 
-  val playerView: PlayerView = itemView.findViewById(R.id.playerView)
+  val playerView: FrameLayout = itemView.findViewById(R.id.playerViewContainer)
 }
